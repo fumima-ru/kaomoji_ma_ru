f6145eee9640eba841070da9e300d7752e36ea6c
fumima-ru <marumefumiko@gmail.com>
2023-02-14 23:08:24 +0900

    更新

--- kaomoji_ma_ru_data.php
+++ kaomoji_ma_ru_data.php
@@ -1,39 +1,97 @@
 <?php
 
 /* '絵文字' 'ペンネーム' 'ジャンル'*/
-/* 喜 joy */
+/*  = new Emoji('', '', ''); */
+/* 喜 joy $joy = new Emoji('', '', 'joy'); */
+$joy = new Emoji('(´>∀<`*)', '', 'joy');
+$joy = new Emoji('(*´ω`*)', '', 'joy');
+$joy = new Emoji('(๑´ㅂ`๑)', '', 'joy');
+$joy = new Emoji('*.。(๑･∀･๑)*.。', '', 'joy');
+$joy = new Emoji('(っ´ω`c)', '', 'joy');
+$joy = new Emoji('\(´ω` )/', '', 'joy');
+$joy = new Emoji('(ﾉ*>∀<)ﾉ', '', 'joy');
+$joy = new Emoji('٩(๑´ω`๑)۶', '', 'joy');
+$joy = new Emoji('(*ฅ´ω`ฅ*)', '', 'joy');
 
-/* 怒 anger */
+/* フフフ・・・ムヒヒ イヒヒ エヘヘ キランッ イタズラ prank $prank = new Emoji('', '', 'prank'); */
+$prank1 = new Emoji('(*´ё`*) ムフフ', '', 'prank');
+$prank = new Emoji('(￣∀￣)', '', 'prank');
+$prank = new Emoji('(￣▽￣)', '', 'prank');
+$prank = new Emoji('(๑¯∇¯๑)', '', 'prank');
+
+/* 怒 anger $anger = new Emoji('', '', 'anger'); */
+$anger1 = new Emoji('(・A・)ｲｸﾅｲ!!', '', 'anger');
 
 /* 哀 sad */
 
 /* 楽 fun */
 
-/* 困る trouble */
+/* 困る trouble $trouble = new Emoji('', '', 'trouble');*/
+$trouble = new Emoji('((∩´o`∩))', '', 'trouble');
+$trouble = new Emoji('(´>_<`)', '', 'trouble');
+$trouble = new Emoji('(´>ω<｀)', '', 'trouble');
 
-/* 驚く surprise */
+/* 驚く surprise $surprise = new Emoji('', '', 'surprise'); */
+$surprise1 = new Emoji('∑(°口°๑)', '', 'surprise');
+$surprise = new Emoji('∑( ◦д⊙)‼', '', 'surprise');
+$surprise = new Emoji('(>×<)', '', 'surprise');
+$surprise = new Emoji('\(°Д° )/', '', 'surprise'); 
+$surprise = new Emoji('Σ╰(°ㅂ° )╯', '', 'surprise');
+$surprise = new Emoji('　　　　あ　あ
+　　あ　　　　　 あ
+   は　　   ぁ　ぁ　    あ
+ぐ　    ぁ　　　っ　    あ
+　     ぁ　 (ﾟДﾟ )・∵　 あ
+　　 ぁ　　　　　　 あ
+　　　ぁ　　　　   あ
+　　　　 ぁ　あ', '', 'surprise');
 
 /* 照れる shy */
+$shy1 = new Emoji('(ฅ∀<`๑)ﾃﾍｯ', '', 'shy');
 
 /* 挨拶 greet */
+$greet1 = new Emoji('(*^o^*)/ ただいまぁ～♪', '', 'greet');
+$greet2 = new Emoji('┏○)) ﾍﾟｺﾘ', '', 'greet');
+
+/* 了解 ok */
+$ok1 = new Emoji('(｀・ω・´)ゞ シャキーン', '', 'ok');
 
 /* ありがとう thank_you */
 $thank_you1 = new Emoji('アリガ━(ﾟ∀ﾟ)━( ﾟ∀)━( ﾟ)━( )━(ﾟ )━(∀ﾟ )━(ﾟ∀ﾟ)━隊!!!
 ', '', 'thank_you');
 
-/* お笑い comedy */
+/* お笑い comedy $comedy = new Emoji('', '', 'comedy'); */
 $comedy1 = new Emoji('( ˚Д˚)ﾉ なんでやねん(笑)', '', 'comedy');
-/* チラッ peek */
+$comedy2 = new Emoji('(  ﾟД ﾟ)ﾉ なんでやねん(笑)', '', 'comedy');
+$comedy = new Emoji('\( °Д° )( ﾟДﾟ)/', '', 'comedy');
+
+/* ウインク wink $wink = new Emoji('', '', 'wink'); */
+$wink = new Emoji('٩(｡•ω<｡)', '', 'wink');
+
+/* チラッ・逃げる peek $peek = new Emoji('', '', 'peek'); */
+$peek = new Emoji('|д･)', '', 'peek');
+$peek = new Emoji('ε”ε”ε”٩(๑•ㅂ•)۶', '', 'peek');
+$peek = new Emoji('ε=ε=ε=(ﾉ｀・∀・)ﾉ
+', '', 'peek');
 
 /* ？ どこ？ where */
+$where1 = new Emoji('(ﾟДﾟ≡ﾟДﾟ)', '', 'where');
 
-/* フフフ・・・キランッ イタズラ prank */
+/* キター come */
+$come1 = new Emoji('ｷﾀ━━━━(ﾟ∀ﾟ)━━━━!!', '', 'come');
 
-/* ムヒヒ イヒヒ エヘヘ 怪しい suspicious */
+/* バタッ exhaustion $exhaustion = new Emoji('', '', 'exhaustion'); */
+$exhaustion = new Emoji('_(:3｣ ∠)_', '', 'exhaustion');
+$exhaustion = new Emoji('_(:D」L)_', '', 'exhaustion');
+$exhaustion = new Emoji('≡(:D)┿━<', '', 'exhaustion');
+
 
 /* 動物 animal */
 
-/* 記号 */
+/* 線 line $line = new Emoji('', '', 'line');*/
+$line1 = new Emoji('❁〜❁〜❁〜❁〜❁〜❁〜❁〜❁〜❁〜❁', '', 'line');
+$line2 = new Emoji('✻〜✻〜✻〜✻〜✻〜✻〜✻〜✻〜✻〜✻', '', 'line');
+$line3 = new Emoji('*〜*〜*〜*〜*〜*〜*〜*〜*〜*〜*〜*〜*〜', '', 'line');
 
 $emojis = array($thank_you1, $comedy1)
 
